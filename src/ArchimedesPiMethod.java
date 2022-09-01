940cc01f91f75dd00bb38958dbbffcc05a8bfa94
bridgetisvirtuallyhere <bridgetjacosta@gmail.com>
2022-09-01 11:04:33 -0700

    Implemented calculating PI as an algorithm. 9-1-2022 Bridget Acosta

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,27 @@
-import java.util.Scanner;
-
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+
+        //integer number of sides in a polygon
+        int numberOfSides = 8;
+
+        //calculate angle B
+        double angleB = 360.0/numberOfSides;
+
+        //calculate angle A
+        double angleA = 1.0/2.0 * angleB;
+
+        //convert angle A to radians
+        //calculate the length on one triangle base
+        double lengthTriangleBase = 2.0 * Math.sin(Math.toRadians(angleA));
+
+        //get polygon perimeter
+        double perimeterOfPolygon = numberOfSides * lengthTriangleBase;
+
+        //estimate PI
+        double estimatePI = perimeterOfPolygon/2.0;
+
+        System.out.println("Estimate PI: ");
+        System.out.print(estimatePI);
 
     }
 }
